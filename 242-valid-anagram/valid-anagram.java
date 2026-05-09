bffac015f2c491aff51883abfb87a0b33d0c3b21
Arshad Jafri <45098927+ArshadJafri@users.noreply.github.com>
2026-05-09 18:58:23 -0400

    Time: 16 ms (21.40%) | Memory: 46.5 MB (32.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,26 +4,31 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+
         HashMap<Character, Integer> map = new HashMap<>();
 
-        for(char c: s.toCharArray()){
+        for(char c : s.toCharArray()){
             map.put(c, map.getOrDefault(c,0)+1);
+
         }
 
         for(char c: t.toCharArray()){
-
             if(!map.containsKey(c)){
                 return false;
             }
+
             map.put(c, map.get(c)-1);
-            
-            if(map.get(c)==0){
+
+            if(map.get(c) ==0){
                 map.remove(c);
+
             }
+
+
+
         }
+        
 
         return true;
-
-        
     }
 }
