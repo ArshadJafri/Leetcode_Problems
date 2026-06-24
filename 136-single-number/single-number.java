5e5adeecf01565c810cf8953cb63b6fe273b4049
Arshad Jafri <45098927+ArshadJafri@users.noreply.github.com>
2026-06-24 19:49:12 -0400

    Time: 14 ms (14.56%) | Memory: 46.8 MB (64.40%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        
 
         HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int num: nums){
 
+        for(int num: nums){
             map.put(num, map.getOrDefault(num,0)+1);
         }
 
@@ -14,9 +14,7 @@ class Solution {
             }
         }
 
-
         return -1;
-
         
     }
 }
